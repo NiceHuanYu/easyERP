9348601c875f3f7ed3d51eaabe40a6dc2b2ae62a
NiceHuanYu <3308225809@qq.com>
2026-07-17 10:09:25 +0000

    Fix redirection problem

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- 此页面仅做重定向，无内容 -->
+  <div></div>
+</template>
+
+<script setup lang="ts">
+const { isLoggedIn } = useAuth()
+
+if (isLoggedIn.value) {
+  await navigateTo('/dashboard', { replace: true })
+} else {
+  await navigateTo('/login', { replace: true })
+}
+</script>
