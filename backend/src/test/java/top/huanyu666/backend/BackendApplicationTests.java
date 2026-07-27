7590f5a014017e2d6532e1fd110c95597835e859
NiceHuanYu <3308225809@qq.com>
2026-07-27 08:39:31 +0000

    Add backend

---
+++ backend/src/test/java/top/huanyu666/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.huanyu666.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
